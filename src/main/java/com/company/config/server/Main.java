fa6fddfebc6e80e6239f155f78ca8118c6f1aa50
VinhTQ <VinhTQ@vissoft.vn>
2023-08-13 02:17:01 +0700

    .

---
+++ src/main/java/com/company/config/server/Main.java
@@ -0,0 +1,14 @@
+package com.company.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
